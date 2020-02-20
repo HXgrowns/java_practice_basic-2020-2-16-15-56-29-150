87bbe2d7dcda28e39aceb2928d9889287af817d5
huxiao <804156017@qq.com>
2020-02-21 00:09:50 +0800

    [huxiao] {add}: add Collection homework

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -8,9 +10,14 @@ public class Practice1 {
     }
 
     /**
-     *  生成10个0到20的随机整数，要求不能重复
+     * 生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Collection<Integer> randoms = new HashSet<>();
+        Random random = new Random();
+        while (randoms.size() < 10) {
+            randoms.add(random.nextInt(21));
+        }
+        return randoms;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,42 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
-        //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        //for循环
+        List<Integer> numbers = getNumbers();
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + " ");
+        }
+        System.out.println("\n----------------------");
+
+        //增强for循环
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println("\n----------------------");
+
+        //迭代器
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println("\n----------------------");
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i <= 100; i++) {
+            numbers.add(i);
+        }
+        return numbers;
     }
 
 
